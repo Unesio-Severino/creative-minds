8a8065ff90e4143c7e4e19ced9a513a85336f0af
Unesio-Severino <unesio.private@gmail.com>
2022-11-11 11:29:19 +0200

    Styling a Font end page.

---
+++ pages/auth/login.js
@@ -0,0 +1,12 @@
+export default function Login(){
+
+    return(
+        <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+            <h2 className="text-2xl font-medium">Entre Hoje</h2>
+            <div className="p-4">
+                <h3 className="p-4">Entre com uns dos provedores</h3>
+                <button className="text-white bg-cyan-700 w-full font-medium rounded-lg flex align-middle p-4">Entre com Google</button>
+            </div>
+        </div>
+    );
+}
