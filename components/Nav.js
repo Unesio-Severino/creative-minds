c4c3d86a7716b65c97baee3a31b97d4398571239
Unesio-Severino <unesio.private@gmail.com>
2022-11-11 15:11:37 +0200

    Rename components

---
+++ components/Nav.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import {auth} from "../utils/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import Image from 'next/image';
+
+export default function Nav() {
+
+//Chamada da função para ver se o utilizador esta logado ou nao
+    const [user, loading] = useAuthState(auth);
+
+    return (
+        <nav className="flex justify-between items-center py-10">
+            <Link href="/">
+                <button className="text-lg font-medium">Creative Post</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                {!user && (
+                <Link legacyBehavior href={"/auth/login"}>
+                    <a className="py-2 px-4 text-sm bg-cyan-700 text-white rounded-lg font-medium ml-8">Entre Agora </a>
+                </Link>
+                )}
+                {user && (
+
+                    <div className="flex items-center gap-7">
+                        <Link href="/post">
+                            <button className="font-medium bg-cyan-700 text-white py-2 px-4 rounded-md text-sm">
+                                Post
+                            </button>
+                        </Link>
+
+                        <Link href="/dashboard">
+                            <img className="w-12 rounded-full cursor-pointer"
+                                src={user.photoURL}
+                            />
+                        </Link>
+                    </div>
+                )}
+            </ul>
+        </nav>
+    );
+}
