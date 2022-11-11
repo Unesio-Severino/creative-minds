c7d55cf0507b12b3fd471df98e67954eb60c615b
Unesio-Severino <unesio.private@gmail.com>
2022-11-11 13:09:12 +0200

    Creating authentication function for users

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,11 +1,45 @@
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+import {useRouter} from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from  "react";
+
+
 export default function Login(){
+    const route = useRouter();
+    const [user, loading] = useAuthState(auth);
+
+
+//Função para autenticação com a conta google
+const googleProvider = new GoogleAuthProvider();
+const GoogleLogin = async () => {
+    try {
+        const result = await signInWithPopup(auth, googleProvider);
+        route.push("/");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+useEffect(() => {
+    if (user) {
+        route.push("/")
+    } else {
+        console.log("login");
+    }
+}, [user]);
 
     return(
         <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
-            <h2 className="text-2xl font-medium">Entre Hoje</h2>
-            <div className="p-4">
-                <h3 className="p-4">Entre com uns dos provedores</h3>
-                <button className="text-white bg-cyan-700 w-full font-medium rounded-lg flex align-middle p-4">Entre com Google</button>
+            <h2 className="text-2xl font-medium">Entrar</h2>
+            <div className="py-4">
+                <h3 className="py-4">Entre com uns dos provedores</h3>
+                <button onClick={GoogleLogin} className="text-white bg-cyan-700 w-full
+                    font-medium rounded-lg flex align-middle p-4 gap-2">
+                    <FcGoogle className="text-3xl"/>
+                        Entre com Google
+                    </button>
             </div>
         </div>
     );
