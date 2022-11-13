e6769570e25b26f232c4748f2c0902f6dae346e0
Unesio-Severino <unesio.private@gmail.com>
2022-11-13 19:15:07 +0200

    Fix Imports

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,7 +2,11 @@ import { auth } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import Message from '../components/message';
+import { AiFillDelete } from 'react-icons/ai';
+import { BiEditAlt } from 'react-icons/bi';
 
 //Aqui sao feitas verificações para evitar que os usuários nao possam
 //aceder o dashboard sem que estejam logados.
@@ -11,11 +15,19 @@ export default function Dashboard() {
 
     const route = useRouter();
     const [user, loading] = useAuthState(auth);
-
+    const [posts, setPosts] = useState([]);
     //see if user is logged
     const getData = async () => {
         if (loading) return;
         if (!user) return route.push("/auth/login");
+
+        //Função para ver as mensagens de post do usuário logado no dashboard de forma individual.
+        const collectionRef = collection(db, "posts");
+        const q = query(collectionRef, where("user", "==", user.uid));
+        const unsubscribe = onSnapshot(q, (snapshot => {
+            setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        }));
+        return unsubscribe;
     };
 
     //Get users Data
@@ -23,10 +35,25 @@ export default function Dashboard() {
         getData();
     }, [user, loading]);
 
-    return(
+    return (
         <div>
-            <h1>Tuas Postagens</h1>
-            <div>Postagem</div>
+            <h1>Minhas Postagens</h1>
+            <div>
+                {posts.map((post) => {
+                    return <Message {...post} key={post.id}>
+                        <div className="flex gap-4">
+                            <button className="text-red-600 text-sm items-center justify-center gap-2 py-2">
+                                <AiFillDelete className="text-2xl"/> Delete
+                            </button>
+                            <button>
+                                <BiEditAlt className="text-2xl"/> Editar
+                            </button>
+                        </div>
+
+
+                    </Message>;
+                })}
+            </div>
             <button onClick={() => auth.signOut()}>Sair</button>
         </div>
     );
