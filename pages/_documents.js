73f2308bad2ffcd91b1d6bf1d850fa1bbd1d697e
Unesio-Severino <unesio.private@gmail.com>
2022-11-13 19:13:22 +0200

    Fix

--- pages/_documents.js
+++ pages/_documents.js
@@ -5,13 +5,13 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-          <Head />
-         <body>
-      <Main />
-      <NextScript />
-      </body>
-  </Html>
-)
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+                            <Head />
+                            <body>
+                                <Main />
+                                <NextScript />
+                            </body>
+                        </Html>
+                        )
 }
