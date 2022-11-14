6b47d791455073e51d150edbe169df628e595561
Unesio-Severino <unesio.private@gmail.com>
2022-11-14 15:14:07 +0200

    Fixing some error

--- pages/_documents.jsx
+++ pages/_documents.jsx
@@ -1,17 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+export default function Document(){
+
     return (
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
-            <Head />
+            <Head/>
                 <body>
                     <Main />
                     <NextScript />
-                </body>
+                <body/>
         </Html>
     );
 }
