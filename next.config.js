93b9ccad0d52d1317918f4460871b29e9931642c
Unesio-Severino <unesio.private@gmail.com>
2023-01-27 00:08:22 +0200

    fixing

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
   target: "serverless",
 };
 
-// module.exports = nextConfig
-module.exports = {
-  distDir: "out",
-};
+module.exports = nextConfig
+// module.exports = {
+//   distDir: "out",
+// };
